61f9a860442137382c540d3a05579e4191ef9ea5
Alan Harper <alan@aussiegeek.net>
2014-06-30 14:33:07 +1000

    Improve documentation
    Added link to live rdocs, and documented paramters accepted by
    Location and Item

--- lib/temando/location.rb
+++ lib/temando/location.rb
@@ -4,6 +4,17 @@ module Temando
   class Location
     attr_accessor :country, :postcode, :suburb, :contact, :state, :street, :phone1, :phone2, :email, :company
 
+    # @param [Hash] attributes to initialize Location with
+    # @option attributes [String] :suburb Suburb name (mandatory)
+    # @option attributes [String] :postcode Postcode (mandatory)
+    # @option attributes [String] :country Two letteer ISO3166 code (mandatory)
+    # @option attributes [String] :contact Contact Name
+    # @option attributes [String] :state State
+    # @option attributes [String] :street Street
+    # @option attributes [String] :phone1 Contact phone number 1
+    # @option attributes [String] :phone2 Contact phone number 1
+    # @option attributes [String] :email Contact e-mail address
+    # @option attributes [String] :company Contact company
     def initialize(attributes={})
       attributes.each do |key, value|
         self.send("#{key}=".intern, value)
